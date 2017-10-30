7c3f202fa221d05e6e3bad0f0584f9cd46a78ddd
Irene DeVera <irenneeyyy@gmail.com>
2017-10-29 20:48:20 -0700

    Changed Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   skip_before_action :require_login, only: [:login]
-  
+
   def index
     @user = User.find(session[:user_id])
   end
@@ -41,44 +41,11 @@ class SessionsController < ApplicationController
   end
 
   def logout
-
     session[:user_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out!"
 
     redirect_to root_path
-
   end
 
-
-  #
-  # def login
-  #   username = params[:username]
-  #   if username and user = User.find_by(username: username)
-  #     session[:user_id] = user.id
-  #     flash[:status] = :success
-  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-  #   else
-  #     user = User.new(username: username)
-  #     if user.save
-  #       session[:user_id] = user.id
-  #       flash[:status] = :success
-  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-  #     else
-  #       flash.now[:status] = :failure
-  #       flash.now[:result_text] = "Could not log in"
-  #       flash.now[:messages] = user.errors.messages
-  #       render "login_form", status: :bad_request
-  #       return
-  #     end
-  #   end
-  #   redirect_to root_path
-  # end
-  #
-  # def logout
-  #   session[:user_id] = nil
-  #   flash[:status] = :success
-  #   flash[:result_text] = "Successfully logged out"
-  #   redirect_to root_path
-  # end
 end
